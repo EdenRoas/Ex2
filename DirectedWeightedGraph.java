3036523d06d6eedf8a3ed11daa30995e3d08f5c3
Eden Roas <edenroas@gmail.com>
2021-12-06 22:05:56 +0200

    fix2

--- DirectedWeightedGraph.java
+++ DirectedWeightedGraph.java
@@ -1,68 +1,118 @@
 import api.EdgeData;
 import api.NodeData;
 
+import java.util.HashMap;
 import java.util.Iterator;
 
 public class DirectedWeightedGraph implements api.DirectedWeightedGraph {
+    private HashMap<Integer,NodeData> NodeMap;
+    private int MC = 0;
+    private HashMap<String, EdgeData> EdgeMap;
 
 
     @Override
     public NodeData getNode(int key) {
+        if(NodeMap.containsKey(key))
+        {
+            return NodeMap.get(key);
+        }
         return null;
     }
 
     @Override
     public EdgeData getEdge(int src, int dest) {
+        String kodkod = src +", "+dest;
+        if(EdgeMap.containsKey(kodkod))
+        {
+            return EdgeMap.get(kodkod);
+        }
         return null;
     }
 
     @Override
     public void addNode(NodeData n) {
-
+        if(n!=null)
+        {
+            NodeMap.put(n.getKey(),n);
+            this.MC++;
+        }
     }
 
     @Override
     public void connect(int src, int dest, double w) {
-
+        if( src == dest || !this.NodeMap.containsKey(src) || !this.NodeMap.containsKey(dest) || getEdge(src, dest) != null )
+            return;
+        EdgeData edge = new EdgeData(src, dest, w);
+        String kodkod = src +", "+dest;
+        EdgeMap.put(kodkod,edge);
+        this.MC++;
     }
 
     @Override
     public Iterator<NodeData> nodeIter() {
-        return null;
-    }
+//        try {
+//            return this.NodeMap.values().iterator();
+//        }
+//        catch (RuntimeException)
+//        {
+//            throw RuntimeException;
+//        }
+
+//    }
 
     @Override
     public Iterator<EdgeData> edgeIter() {
-        return null;
+        return this.EdgeMap.values().iterator();
     }
 
     @Override
     public Iterator<EdgeData> edgeIter(int node_id) {
-        return null;
+        return this.EdgeMap.values().iterator();
     }
 
     @Override
     public NodeData removeNode(int key) {
-        return null;
+        if(!this.NodeMap.containsKey(key))
+            return null;
+        //if(! this.NodeMap.values().isEmpty()){
+        for (NodeData n : this.NodeMap.values()) {
+            if(n.getKey() == key)
+                continue;
+            String kod1 = key +", "+n.getKey();;
+            String kod2 = n.getKey() +", "+key;
+            if(this.EdgeMap.containsKey(kod1))
+                EdgeMap.remove(kod1);
+            if(this.EdgeMap.containsKey(kod2))
+                EdgeMap.remove(kod2);
+        }
+        this.MC++;
+        return NodeMap.remove(key);
     }
 
     @Override
     public EdgeData removeEdge(int src, int dest) {
-        return null;
+        String kodkod = src +", "+dest;
+
+        if(!EdgeMap.containsKey(kodkod))
+            return null;
+
+        this.MC++;
+
+        return this.EdgeMap.remove(kodkod);
     }
 
     @Override
     public int nodeSize() {
-        return 0;
+        return NodeMap.size();
     }
 
     @Override
     public int edgeSize() {
-        return 0;
+        return EdgeMap.size();
     }
 
     @Override
     public int getMC() {
-        return 0;
+        return this.MC;
     }
 }
