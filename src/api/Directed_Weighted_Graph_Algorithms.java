76fd72217fdac6e2d0161b3a214a3479f08a0592
Eden Roas <92533182+EdenRoas@users.noreply.github.com>
2021-12-07 23:32:53 +0200

    Add files via upload

--- src/api/Directed_Weighted_Graph_Algorithms.java
+++ src/api/Directed_Weighted_Graph_Algorithms.java
@@ -1,23 +1,36 @@
 package api;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 //import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class Directed_Weighted_Graph_Algorithms implements DirectedWeightedGraphAlgorithms{
+import com.google.gson.JsonParser;
+//import org.json.JSONArray;
+//import org.json.JSONObject;
+//import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 
-    DirectedWeightedGraph graph;
+
+import java.io.*;
+
+public class Directed_Weighted_Graph_Algorithms implements DirectedWeightedGraphAlgorithms {
+
+    Directed_Weighted_Graph graph;
     double[][] dist = new double[this.graph.nodeSize()][this.graph.nodeSize()];
     NodeData[][] next = new Node_Data[this.graph.nodeSize()][this.graph.nodeSize()];
 
-    public Directed_Weighted_Graph_Algorithms(DirectedWeightedGraph g)
-    {
+    public Directed_Weighted_Graph_Algorithms(DirectedWeightedGraph g) {
         init(g);
     }
 
     @Override
     public void init(DirectedWeightedGraph g) {
-        this.graph = g;
+        this.graph = (Directed_Weighted_Graph) g;
     }
 
     @Override
@@ -28,16 +41,14 @@ public class Directed_Weighted_Graph_Algorithms implements DirectedWeightedGraph
     @Override
     public DirectedWeightedGraph copy() {
         DirectedWeightedGraph copy_graph = new Directed_Weighted_Graph();
-        Directed_Weighted_Graph g = (Directed_Weighted_Graph)this.graph;
+        Directed_Weighted_Graph g = (Directed_Weighted_Graph) this.graph;
         HashMap<Integer, NodeData> new_node_map = g.getNodeMap();
         HashMap<String, EdgeData> new_edge_map = g.getEdgeMap();
-        for (NodeData n : new_node_map.values())
-        {
+        for (NodeData n : new_node_map.values()) {
             NodeData temp = new Node_Data(n.getKey(), n.getLocation(), n.getWeight(), n.getInfo(), n.getTag());
             copy_graph.addNode(temp);
         }
-        for(EdgeData edge : new_edge_map.values())
-        {
+        for (EdgeData edge : new_edge_map.values()) {
             copy_graph.connect(edge.getSrc(), edge.getDest(), edge.getWeight());
         }
         return copy_graph;
@@ -45,19 +56,17 @@ public class Directed_Weighted_Graph_Algorithms implements DirectedWeightedGraph
 
     @Override
     public boolean isConnected() {
-        return this.graph.edgeSize() == (this.graph.nodeSize()*(this.graph.nodeSize() - 1));    }
+        return this.graph.edgeSize() == (this.graph.nodeSize() * (this.graph.nodeSize() - 1));
+    }
 
-    private void FWA_with_PR()
-    {
-        for(int i=0; i<this.dist.length; i++)
-        {
-            for(int j=0; j<this.dist.length; j++)
-            {
-                if(i == j)
+    private void FWA_with_PR() {
+        for (int i = 0; i < this.dist.length; i++) {
+            for (int j = 0; j < this.dist.length; j++) {
+                if (i == j)
                     this.dist[i][j] = 0;
                 else {
                     EdgeData tmp = this.graph.getEdge(i, j);
-                    if(tmp != null)
+                    if (tmp != null)
                         dist[i][j] = tmp.getWeight();
                     else
                         dist[i][j] = Integer.MAX_VALUE;
@@ -65,13 +74,10 @@ public class Directed_Weighted_Graph_Algorithms implements DirectedWeightedGraph
                 next[i][j] = this.graph.getNode(i);
             }
         }
-        for(int k=0; k<this.graph.nodeSize(); k++)
-        {
-            for(int i=0; i<this.graph.nodeSize(); i++)
-            {
-                for(int j=0; j<this.graph.nodeSize(); j++)
-                {
-                    if(this.dist[i][j] > this.dist[i][k] + this.dist[k][j]) {
+        for (int k = 0; k < this.graph.nodeSize(); k++) {
+            for (int i = 0; i < this.graph.nodeSize(); i++) {
+                for (int j = 0; j < this.graph.nodeSize(); j++) {
+                    if (this.dist[i][j] > this.dist[i][k] + this.dist[k][j]) {
                         this.dist[i][j] = this.dist[i][k] + this.dist[k][j];
                         next[i][j] = next[i][k];
                     }
@@ -93,11 +99,10 @@ public class Directed_Weighted_Graph_Algorithms implements DirectedWeightedGraph
         int tmp_dest = dest;
         NodeData tmp_node = this.graph.getNode(src);
         List<NodeData> path = new ArrayList<>();
-        if(next[src][dest] == null)
+        if (next[src][dest] == null)
             return null;
         path.add(tmp_node);
-        while(tmp_node.getKey() != tmp_dest)
-        {
+        while (tmp_node.getKey() != tmp_dest) {
             tmp_node = next[tmp_node.getKey()][tmp_dest];
             path.add(tmp_node);
         }
@@ -125,8 +130,8 @@ public class Directed_Weighted_Graph_Algorithms implements DirectedWeightedGraph
         Integer[] centers_array = new Integer[centers_list.size()];
         centers_array = centers_list.toArray(centers_array);
         int min = centers_array[0];
-        for(int i = 1; i < centers_array.length; i++){
-            if(centers_array[i] < min)
+        for (int i = 1; i < centers_array.length; i++) {
+            if (centers_array[i] < min)
                 min = centers_array[i];
         }
         return this.graph.getNode(min);
@@ -139,11 +144,54 @@ public class Directed_Weighted_Graph_Algorithms implements DirectedWeightedGraph
 
     @Override
     public boolean save(String file) {
-        return false;
-    }
+        try {
+            JSONObject file_g = new JSONObject();
+            file_g.put("Edges" ,this.graph.getEdgeMap());
+            file_g.put("Nodes" ,this.graph.getNodeMap());
+            FileWriter writer = new FileWriter(file);
+            writer.write(file_g.toJSONString());
+            writer.flush();
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
 
     @Override
     public boolean load(String file) {
-        return false;
+
+        try {
+            Directed_Weighted_Graph dg = new Directed_Weighted_Graph();
+            JsonParser jsonpars = new JsonParser();
+            Object ob = jsonpars.parse(new FileReader(file));
+            JSONObject file_g = (JSONObject) ob;
+            JSONArray ed = (JSONArray) file_g.get("Edges");
+            for (Object o : ed) {
+                HashMap<String, Object> edgedata = (HashMap<String, Object>) o;
+                int src = (int) (long) edgedata.get("src");
+                int dest = (int) (long) edgedata.get("dest");
+                double w = (double) edgedata.get("w");
+                dg.connect(src, dest, w);
+            }
+            JSONArray nod = (JSONArray) file_g.get("Nodes");
+            for (Object oi : nod) {
+                HashMap<String, Object> nodedata = (HashMap<String, Object>) oi;
+                String pos = (String) nodedata.get("pos");
+                String[] spliti = pos.split(",");
+                Geo_Location g = new Geo_Location(Double.parseDouble(spliti[0]), Double.parseDouble(spliti[1]), Double.parseDouble(spliti[2]));
+                int id = (int) (long) nodedata.get("id");
+                Node_Data nv = new Node_Data(id, g, 0, "", 0);
+                dg.addNode(nv);
+            }
+            this.init(dg);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
+
+
 }
+
