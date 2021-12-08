abae24ef0504679969f17787eeac88d30542b0f8
Eden Roas <92533182+EdenRoas@users.noreply.github.com>
2021-12-08 14:34:39 +0200

    Add files via upload

---
+++ Tests/Directed_Weighted_GraphTest.java
@@ -0,0 +1,68 @@
+package Tests.Tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Directed_Weighted_GraphTest {
+
+    @Test
+    void getNodeMap() {
+    }
+
+    @Test
+    void getEdgeMap() {
+    }
+
+    @Test
+    void getNode() {
+    }
+
+    @Test
+    void getEdge() {
+    }
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void getCenter() {
+    }
+
+    @Test
+    void connect() {
+    }
+
+    @Test
+    void nodeIter() {
+    }
+
+    @Test
+    void edgeIter() {
+    }
+
+    @Test
+    void testEdgeIter() {
+    }
+
+    @Test
+    void removeNode() {
+    }
+
+    @Test
+    void removeEdge() {
+    }
+
+    @Test
+    void nodeSize() {
+    }
+
+    @Test
+    void edgeSize() {
+    }
+
+    @Test
+    void getMC() {
+    }
+}
